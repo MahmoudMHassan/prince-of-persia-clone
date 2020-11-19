819906c872bcf3e0610b5b60c2c3715ce5d53d13
Mahmoud Hassan <dragonxyz94@gmail.com>
2020-11-19 21:40:00 +0100

    Upload Source Code

---
+++ Assets/Animation/MotionSMB.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MotionSMB : StateMachineBehaviour {
+	public float m_Damping=0.15f;
+	private readonly int m_HashHorzPara=Animator.StringToHash("Horizontal");
+private readonly int m_HashVerticalPara=Animator.StringToHash("Vertical");
+	 // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+	//override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+
+	// OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+	override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+//	if(Input.GetKeyDown("j")){
+//	Vector2 input1=new Vector2(1,1).normalized;
+//	animator.SetFloat(m_HashHorzPara,input1.x,m_Damping,15*Time.deltaTime);
+//	animator.SetFloat(m_HashVerticalPara,input1.y,m_Damping,15*Time.deltaTime);
+//		return;
+//	}
+
+	float hor=Input.GetAxis("Horizontal");
+	float ver=Input.GetAxis("Vertical");
+	//if(Input.GetKeyDown("w")){
+	
+     // rigd.AddForce ( * speed);
+       //Debug.Log("ahahhaah");
+        //Vector3 input =new Vector3(0f, 0.0f, 1f);
+	//console.print("w");
+	Vector2 input=new Vector2(hor,ver).normalized;
+	//animator.SetFloat(m_HashHorzPara,input.x,m_Damping,15*Time.deltaTime);
+	animator.SetFloat(m_HashVerticalPara,input.y,m_Damping,15*Time.deltaTime);
+		//}
+
+
+
+		
+	
+	}
+
+	// OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+	override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//	 Debug.Log("ehdah:");
+	//animator.SetFloat(m_HashHorzPara,0.0f,m_Damping,15*Time.deltaTime);
+	//animator.SetFloat(m_HashVerticalPara,0.0f,m_Damping,15*Time.deltaTime);
+	}
+
+	// OnStateMove is called right after Animator.OnAnimatorMove(). Code that processes and affects root motion should be implemented here
+	//override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+
+	// OnStateIK is called right after Animator.OnAnimatorIK(). Code that sets up animation IK (inverse kinematics) should be implemented here.
+	//override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+	//
+	//}
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+	Animator m_Animator;
+	bool m_Attack;
+	bool m_Run;
+	bool m_Jump;
+	float m_StationaryTurnSpeed = 180;
+	float m_MovingTurnSpeed = 360;
+	float m_ForwardAmount;
+	private Vector3 m_CamForward;    
+	private Vector3 m_Move;
+	private Transform m_Cam; 
+	float m_TurnAmount;
+	float speed = 100f;
+	// Use this for initialization
+	void Start () {
+		m_Animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		m_Attack = Input.GetKeyDown(KeyCode.L);
+		m_Run = Input.GetKey(KeyCode.LeftShift);
+		m_Jump = Input.GetKeyDown(KeyCode.Space);
+		ApplyExtraTurnRotation ();
+		m_Animator.SetBool("Attack", m_Attack);
+		m_Animator.SetBool("Run", m_Run);
+		m_Animator.SetBool ("Jump", m_Jump);
+		if(m_Attack) {
+			//print ("attack");
+			//gameObject.transform.Rotate(180* Time.deltaTime,180* Time.deltaTime, 180* Time.deltaTime);
+			//m_Animator.SetBool("Attack", false);
+
+		}
+	}
+
+
+	void ApplyExtraTurnRotation()
+	{
+		// help the character turn faster (this is in addition to root rotation in the animation)
+		//float turnSpeed = Mathf.Lerp(m_StationaryTurnSpeed, m_MovingTurnSpeed, m_ForwardAmount);
+		//transform.Rotate(0, m_TurnAmount * turnSpeed * Time.deltaTime, 0);
+		transform.Rotate(0, Input.GetAxis("Horizontal")*Time.deltaTime*speed, 0);
+
+
+		//transform.Rotate(180,180, 180);
+
+	}
+
+}
